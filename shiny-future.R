31c068d86078cf5f3e982b67ab5250dfbc07c98e
Timothy Mastny <tim.mastny@gmail.com>
2019-04-06 10:15:22 -0500

    delete unused files

--- shiny-future.R
+++
@@ -1,6 +0,0 @@
-# Since accessing return values of futures resume blocking,
-# futures are most useful for side-effects.
-#
-# We typically do not care (within reason) when a side-effect
-# takes place. But we don't want the side-effect to delay
-# over components of the application.
